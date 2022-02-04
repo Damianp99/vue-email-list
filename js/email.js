e06d10ae0dc94c19cb16fedf9ba6909f906f5372
Damianp99 <damianperez200199@gmail.com>
2022-02-04 16:38:59 +0100

    added random emails with axios

--- js/email.js
+++ js/email.js
@@ -13,6 +13,25 @@ Vue.config.devtools = true
 const app = new Vue({
     el: '#root',
     data: {
+        emails: [],
+        totalEmails: 10
+    },
+    methods: {
+        getRandomEmail() {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(res => {
+                this.emails.push(res.data.response)
+            });
+        },
+        getRandomEmails() {
+            for (let i = 0; i < this.totalEmails; i++) {
+                this.getRandomEmail(this.emails);
+            };
+        },
 
+    },
+    created() {
+        this.getRandomEmails();
     }
+
+
 });
